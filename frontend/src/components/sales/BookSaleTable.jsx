6f3ed9d36d47adae8ee8dd88ac1e83333551d31d
Arl Kheem Galario <arlkh33m@gmail.com>
2023-11-30 15:53:44 +0800

    bookSale Page done in table

---
+++ frontend/src/components/sales/BookSaleTable.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+
+const BookSaleTable = ({ books, sales }) => {
+    const totalQuantities = {};
+
+
+    sales.forEach((sale) => {
+        if (!totalQuantities[sale.book_id]) {
+            totalQuantities[sale.book_id] = 0;
+        }
+        totalQuantities[sale.book_id] += sale.quantity;
+        //console.log(sale.book_id);
+    });
+
+    // Calculate total quantities for each book
+
+
+    return (
+        <table className='w-full border-separate border-spacing-2'>
+            <thead>
+                <tr>
+                    <th className='border border-slate-600 rounded-mb'> No</th>
+                    <th className='border border-slate-600 rounded-mb '> Title</th>
+                    <th className='border border-slate-600 rounded-mb max-md:hidden'> Remaining Stock </th>
+                    <th className='border border-slate-600 rounded-mb max-md:hidden'> Sales</th>
+                    <th className='border border-slate-600 rounded-mb'> Operations</th>
+                </tr>
+            </thead>
+            <tbody>
+                {books.map((book, index) => (
+                    <tr key={book._id} className='h-8'>
+                        <td className='border border-slate-700 rounded-mb text-center'>
+                            {index + 1}
+                        </td>
+                        <td className='border border-slate-700 rounded-mb text-center'>
+                            {book.title}
+                        </td>
+                        <td className='border border-slate-700 rounded-mb text-center font-bold text-blue-800 max-md:hidden'>
+                            {book.stock}
+                        </td>
+                        <td className='border border-slate-700 rounded-mb text-center max-md:hidden'>
+                            {totalQuantities[book._id] || 0}
+
+                        </td>
+                        <td className='border border-slate-700 rounded-mb text-center'>
+                            <div className='flex justify-center gap-x-4'>
+                                <Link to={`books/details/${book._id}`}>
+                                    <BsInfoCircle className='text-2xl text-green-800' />
+                                </Link>
+                                <Link to={`books/edit/${book._id}`}>
+                                    <AiOutlineEdit className='text-2xl text-yellow-600' />
+                                </Link>
+                                <Link to={`books/delete/${book._id}`}>
+                                    <MdOutlineDelete className='text-2xl text-red-600' />
+                                </Link>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+
+}
+
+export default BookSaleTable
---
+++ frontend/src/pages/HomeSales.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Spinner from '../components/Spinner'
+import { Link } from 'react-router-dom'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+
+import Search from '../components/Search'
+import BookSaleTable from '../components/sales/BookSaleTable'
+
+const HomeSales = () => {
+    const [numberOfData,setNumberOfData] = useState();
+    const [books, setBooks] = useState([]);
+    const [sales, setSales] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showType, setShowType] = useState('table');
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:5555/books');
+            setBooks(response.data.data);
+            const x = response.data.data;
+            setNumberOfData(x.length);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
+
+    const fetchSalesData = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get('http://localhost:5555/bookSale');
+            setSales(response.data.data);
+            //console.log(response.data.data);
+            const x = response.data.data;
+            setNumberOfData(x.length);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+        fetchSalesData();
+    }, []);
+
+    useEffect(() => {
+        const handleSearch = async () => {
+            try {
+                //console.log('Search Term:', searchTerm);
+                if (searchTerm.trim() === '') {
+                   // console.log('Fetching original data...');
+                    fetchData(); // If search term is empty, fetch original data
+                } else {
+                    setLoading(true);
+                    const response = await axios.get(`http://localhost:5555/books`);
+                    const filteredBooks = response.data.data.filter((book) =>
+                        book.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+                        book.author.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+                        book.category.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+                        book.publishYear.toString().indexOf(searchTerm) !== -1
+                        
+                    );
+                    
+                    setBooks(filteredBooks);
+                    setNumberOfData(filteredBooks.length);
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        handleSearch();
+    }, [searchTerm]);
+    return (
+        <div className='p-4'>
+            <div className='flex flex-col md:flex-row justify-center items-center gap-y-4 md:gap-x-4'>
+                <Search onSearch={setSearchTerm} />
+                {/* <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+                    onClick={() => setShowType('table')}
+                >
+                    Card
+                </button>
+                <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+                    onClick={() => setShowType('card')}
+                >
+                    Table
+                </button> */}
+
+            </div>
+            <div className='flex justify-between items-center'>
+                <h1 className='text-3xl my-10'>Book Inventory : {numberOfData}</h1>
+                {/* <Link to='books/create' className='flex items-center gap-2'>
+                    <MdOutlineAddBox className='text-sky-800 text-4xl' />
+                    <span className='text-xl'>Add Book</span>
+                </Link> */}
+            </div>
+            {loading ? (
+                <Spinner />
+            ) : (
+
+               <BookSaleTable books={books} sales={sales}/>
+
+            )}
+        </div>
+    )
+}
+
+export default HomeSales
